feb18772ade9e360032b190e610bfc4d63718b7b
yana <yanazl@ac.sce.ac.il>
2024-12-15 13:56:03 +0200

    db tables

--- Ebook_Libary_project/Userdatabase.cs
+++ Ebook_Libary_project/Userdatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,7 @@ namespace Ebook_Libary_project
 {
     public class Userdatabase
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["defaultConnectionString"].ConnectionString;
+
     }
 }
